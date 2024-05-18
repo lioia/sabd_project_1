611bcb608421a876787202f3141005e2ba471294
Alessandro Lioi <lioialessandro2000@live.com>
2024-05-18 12:17:03 +0200

    feat(python): second query (not tested)

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 from operator import add
 
 
-def add_header(header: str, index: int, itr: Iterable[Tuple[Tuple[str, str], int]]):
+def add_header(header: str, index: int, itr: Iterable):
     if index == 0:
         yield header
     yield from itr
@@ -16,6 +16,8 @@ def add_header(header: str, index: int, itr: Iterable[Tuple[Tuple[str, str], int
 # - [2]: model
 # - [3]: failure
 # - [4]: vault_id
+
+
 def query_1(rdd: RDD[Tuple[str, str, str, int, str]]):
     # mapping into ((date, vault_id), failures) and reducing by (date, vault_id)
     output_rdd = (
@@ -28,13 +30,67 @@ def query_1(rdd: RDD[Tuple[str, str, str, int, str]]):
         # sorting by value, then by date, then by vault_id
         .sortBy(lambda x: (-x[1], x[0][0], x[0][1]))  # type: ignore[type-var]
     )
-    # TODO: save into hdfs (currently printing just to check if it works)
-    # TODO: execution time
-    header = "DD-MM-YYYY,vault id,count"
+    header = "# DD-MM-YYYY,vault id,count"
     # add header to the data and save it to HDFS
     output_rdd.mapPartitionsWithIndex(
         lambda i, itr: add_header(header, i, itr)
     ).saveAsTextFile("hdfs://hdsf_master/results/query_1.csv")
+
+
+def query_2(spark: SparkSession, rdd: RDD[Tuple[str, str, str, int, str]]):
+    ranking_1 = (
+        # mapping into (model, failure)
+        rdd.map(lambda x: (x[2], x[3]))
+        # summing all the failures for the specific model
+        .reduceByKey(add)
+        # sorting by the failure count
+        .sortBy(lambda x: -x[1])  # type: ignore[type-var]
+    )
+
+    vault_failures = (
+        # mapping into (vault_id, failures)
+        rdd.map(lambda x: (x[4], x[3]))
+        # summing all the failures in the vault
+        .reduceByKey(add)
+        # sorting by the number of failures
+        .sortBy(lambda x: -x[1])  # type: ignore[type-var]
+    )
+    vault_model_failures = (
+        # filtering the models without a failure
+        rdd.filter(lambda x: x[3] > 0)
+        # mapping into (vault_id, model)
+        .map(lambda x: (x[4], x[2]))
+        # grouping into (vault_id, list_of_models)
+        .groupByKey()
+        # transform Spark Iterable into an actual list
+        .mapValues(list)
+    )
+
+    header_ranking_1 = "# model,failures_count"
+    output_ranking_1 = (
+        # mapping into the same structure as the other ranking
+        ranking_1.map(lambda x: (x[0], x[1], None))
+        # adding header
+        .mapPartitionsWithIndex(lambda i, itr: add_header(header_ranking_1, i, itr))
+        # taking the first 10 (+1 header)
+        .take(11)
+    )
+
+    header_ranking_2 = "# vault_id,failures_count,list_of_models"
+    output_ranking_2 = (
+        # joining the tww RDDs into (vault_id, (failures_count, list_of_models))
+        vault_failures.join(vault_model_failures)
+        # flatten into (vault_id, failures_count, list_of_models)
+        .map(lambda x: (x[0], x[1][0], x[1][1]))
+        # adding header
+        .mapPartitionsWithIndex(lambda i, itr: add_header(header_ranking_2, i, itr))
+        # taking the first 10 (+1 header)
+        .take(11)
+    )
+    spark.sparkContext.parallelize([output_ranking_1, output_ranking_2]).saveAsTextFile(
+        "hdfs://hdsf_master/results/query_2.csv"
+    )
+    pass
 
 
 def main():
@@ -45,7 +101,9 @@ def main():
         .map(lambda x: (x[0][:10], x[1], x[2], int(x[3]), x[4]))
         .cache()
     )
+    # TODO: calculate execution time for both queries
     query_1(rdd)
+    query_2(spark, rdd)
     spark.stop()
 
 

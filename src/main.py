24f5242e5c182ed1849a1c1dffeae47038eb1365
Alessandro Lioi <lioialessandro2000@live.com>
2024-05-18 11:07:03 +0200

    feat(python): finished first query (not tested on cluster)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from typing import Tuple
+from typing import Tuple, Iterable
 from pyspark import RDD
-from pyspark.sql import Row, SparkSession
+from pyspark.sql import SparkSession
 from operator import add
+
+
+def add_header(header: str, index: int, itr: Iterable[Tuple[Tuple[str, str], int]]):
+    if index == 0:
+        yield header
+    yield from itr
 
 
 # Tuple:
@@ -10,46 +16,36 @@ from operator import add
 # - [2]: model
 # - [3]: failure
 # - [4]: vault_id
-def map_filter_row(row: Row) -> Tuple:
-    split = row.__str__().split(",")
-    date = split[0][:10]
-    serial_number = split[1]
-    model = split[2]
-    failure = int(split[3])
-    vault_id = split[4]
-    return (date, serial_number, model, failure, vault_id)
-
-
-def query_1(rdd: RDD):
+def query_1(rdd: RDD[Tuple[str, str, str, int, str]]):
     # mapping into ((date, vault_id), failures) and reducing by (date, vault_id)
-    reduced_rdd = rdd.map(lambda x: ((x[0], x[4]), x[3])).reduceByKey(add).cache()
-    # filtering based on failures
-    failures_4 = reduced_rdd.filter(lambda x: x[1] == 4).collect()
-    failures_3 = reduced_rdd.filter(lambda x: x[1] == 3).collect()
-    failures_2 = reduced_rdd.filter(lambda x: x[1] == 2).collect()
+    output_rdd = (
+        # mapping into ((date, vault_id), failures)
+        rdd.map(lambda x: ((x[0], x[4]), x[3]))
+        # reducing by key (date, vault_id)
+        .reduceByKey(add)
+        # filtering based on requested values
+        .filter(lambda x: x[1] == 4 or x[1] == 3 or x[1] == 2)
+        # sorting by value, then by date, then by vault_id
+        .sortBy(lambda x: (-x[1], x[0][0], x[0][1]))  # type: ignore[type-var]
+    )
     # TODO: save into hdfs (currently printing just to check if it works)
     # TODO: execution time
-    print("Failure 4")
-    for key, value in failures_4:
-        print(f"{key[0]},{key[1]},{value}")
-    print("Failure 3")
-    for key, value in failures_3:
-        print(f"{key[0]},{key[1]},{value}")
-    print("Failure 2")
-    for key, value in failures_2:
-        print(f"{key[0]},{key[1]},{value}")
+    header = "DD-MM-YYYY,vault id,count"
+    # add header to the data and save it to HDFS
+    output_rdd.mapPartitionsWithIndex(
+        lambda i, itr: add_header(header, i, itr)
+    ).saveAsTextFile("hdfs://hdsf_master/results/query_1.csv")
 
 
 def main():
     spark = SparkSession.Builder().appName("SABDProject1").getOrCreate()
-    rdd = spark.read.text("hdfs://hdsf_master/data/dataset.csv").rdd
-    header = rdd.first()  # getting header line
-
-    # filtering header and selecting the important values
-    # caching the result as it will be used for the two queries
-    # (by caching, it is not recalculated each time)
-    filtered_rdd = rdd.filter(lambda x: x != header).map(map_filter_row).cache()
-    query_1(filtered_rdd)
+    rdd = (
+        spark.read.csv("hdfs://hdfs_master/data/dataset.csv")
+        .rdd.filter(lambda x: x[4].isdecimal())
+        .map(lambda x: (x[0][:10], x[1], x[2], int(x[3]), x[4]))
+        .cache()
+    )
+    query_1(rdd)
     spark.stop()
 
 

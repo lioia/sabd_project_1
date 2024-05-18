c89a86f9e8c4cd9519c3f8387c516d989c1fff14
Alessandro Lioi <lioialessandro2000@live.com>
2024-05-18 12:18:21 +0200

    fix(python): comments

--- src/main.py
+++ src/main.py
@@ -19,7 +19,6 @@ def add_header(header: str, index: int, itr: Iterable):
 
 
 def query_1(rdd: RDD[Tuple[str, str, str, int, str]]):
-    # mapping into ((date, vault_id), failures) and reducing by (date, vault_id)
     output_rdd = (
         # mapping into ((date, vault_id), failures)
         rdd.map(lambda x: ((x[0], x[4]), x[3]))
@@ -30,8 +29,8 @@ def query_1(rdd: RDD[Tuple[str, str, str, int, str]]):
         # sorting by value, then by date, then by vault_id
         .sortBy(lambda x: (-x[1], x[0][0], x[0][1]))  # type: ignore[type-var]
     )
-    header = "# DD-MM-YYYY,vault id,count"
     # add header to the data and save it to HDFS
+    header = "# DD-MM-YYYY,vault id,count"
     output_rdd.mapPartitionsWithIndex(
         lambda i, itr: add_header(header, i, itr)
     ).saveAsTextFile("hdfs://hdsf_master/results/query_1.csv")
@@ -47,6 +46,7 @@ def query_2(spark: SparkSession, rdd: RDD[Tuple[str, str, str, int, str]]):
         .sortBy(lambda x: -x[1])  # type: ignore[type-var]
     )
 
+    # Ranking 2
     vault_failures = (
         # mapping into (vault_id, failures)
         rdd.map(lambda x: (x[4], x[3]))
@@ -87,6 +87,7 @@ def query_2(spark: SparkSession, rdd: RDD[Tuple[str, str, str, int, str]]):
         # taking the first 10 (+1 header)
         .take(11)
     )
+    # merge the two rankings and save into HDFS
     spark.sparkContext.parallelize([output_ranking_1, output_ranking_2]).saveAsTextFile(
         "hdfs://hdsf_master/results/query_2.csv"
     )
@@ -96,9 +97,14 @@ def query_2(spark: SparkSession, rdd: RDD[Tuple[str, str, str, int, str]]):
 def main():
     spark = SparkSession.Builder().appName("SABDProject1").getOrCreate()
     rdd = (
+        # read from HDFS
         spark.read.csv("hdfs://hdfs_master/data/dataset.csv")
+        # filter all the headers (every ~60k events there is a header)
         .rdd.filter(lambda x: x[4].isdecimal())
+        # mapping into (date, serial_number, model, failure, vault_id)
+        # date is truncated into the format YYYY-MM-DD
         .map(lambda x: (x[0][:10], x[1], x[2], int(x[3]), x[4]))
+        # caching as it is required by the two queries
         .cache()
     )
     # TODO: calculate execution time for both queries
